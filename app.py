a015c55b4d186bd35e3c3283e6d3ad06d15c62ad
Ubond-edu <132318268+Ubond-edu@users.noreply.github.com>
2023-07-19 16:28:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
-from langchain.chains import RetrievalQAWithSourcesChain  
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
 import streamlit as st
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
-import pinecone
 
 load_dotenv()
 
@@ -13,22 +12,23 @@ PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment=os.environ["PINECONE_ENV"])
-
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-
 index_name = "ask dr bob"
+
+# Initialize Pinecone with your existing index name
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+
 # Create OpenAIEmbeddings instance
 embeddings = OpenAIEmbeddings()
 
-# Initialize Pinecone with your existing index name
+# Initialize Pinecone vector store with the existing index
 doc_db = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
 
 llm = ChatOpenAI(
     openai_api_key=OPENAI_API_KEY,
-    model_name='gpt-3.5-turbo',  # Adjust model_name as necessary
-    temperature=0.2  # Adjust temperature as necessary
+    model_name='gpt-3.5-turbo',
+    temperature=0.2
 )
+
 
 def retrieval_answer_with_sources(query):
     qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
@@ -57,6 +57,7 @@ def main():
                 st.info("Sources: " + ", ".join(sources))
             else:
                 st.info("No sources found.")
-            
+
+
 if __name__ == "__main__":
     main()
